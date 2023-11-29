8ae78ab1cba20ebdffe79cb18d29bf292109e778
YousifReda <yousifreda2001@gmail.com>
2023-11-29 11:10:14 +0200

    new1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mini_pc/test.dart';
+import 'package:mini_pc/screens/homeScreen/view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyWebSocketPage(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/homeScreen/view/home_screen.dart
+++ lib/screens/homeScreen/view/home_screen.dart
@@ -1,83 +1,185 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mini_pc/components/texts.dart';
 import 'package:mini_pc/screens/homeScreen/widgets/change_massage_dialog.dart';
 import 'package:mini_pc/screens/homeScreen/widgets/error_massege_dialog.dart';
 import 'package:mini_pc/screens/homeScreen/widgets/success_massage_dialog.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-class HomeScreens extends StatelessWidget {
-  const HomeScreens({super.key});
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final WebSocketChannel channel;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // String url =
+    //     'ws://51.20.73.175:8000/ws/open_cell_employee/';
+    String url = 'ws://127.0.0.1:8000/ws/open_cell_employee/';
+    final urlChanged = Uri.parse(url);
+    channel = WebSocketChannel.connect(urlChanged);
+    channel.stream.handleError((error) {
+      // Print the error if it occurs during the WebSocket stream
+      print('WebSocket stream error: $error');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Image(
-                image: const AssetImage(
-                  "assets/images/imbox.png",
-                ),
-              ),
-            ),
-            SizedBox(height: 10,),
-            Expanded(
+        child: StreamBuilder(
+          stream: channel.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Map<String, dynamic> jsonData = json.decode(snapshot.data);
+
+              String key = jsonData['data']['key'];
+
+              if (key == 'success_open') {
+                // showSuccessDialog(context, jsonData['data']);
+                WidgetsBinding.instance!.addPostFrameCallback((_) {
+                  showSuccessDialog(context, jsonData['data']);
+                });
+              } else if (key == 'error_no_cell_assigned') {
+                WidgetsBinding.instance!.addPostFrameCallback((_) {
+                  showErrorDialog(context);
+                });
+              } else {
+                WidgetsBinding.instance!.addPostFrameCallback((_) {
+                  showChangeDialog(context, jsonData['data']);
+                });
+              }
+            } else {
+              return Center(
                 child: Column(
-                    children: [
-                      title("Welcome"),
-                      mediumText("Please Enter You ID"),
-                    ],
-                )
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Expanded(
-                        child:  InkWell(
-                          onTap: (){
-                            showErrorDialog(context);
-                          },
-                          child: Image(
-                            image: const AssetImage(
-                              "assets/images/card.png",
-                            ),
-                          ),
-                        ),
-                      ),
-                     Expanded(
-                        child:  InkWell(
-                          onTap: (){
-                            showSuccessDialog(context,"");
-                          },
-                          child: Image(
-                            image: const AssetImage(
-                              "assets/images/phone.png",
-                            ),
-                          ),
-                        ),
-                      ),
-                    Expanded(
-                      child: InkWell(
-                        onTap: (){
-                          showChangeDialog(context,"");
-                        },
-                        child: Image(
-                          image: const AssetImage(
-                            "assets/images/finger_print.jpg",
-                          ),
+                      child: Image(
+                        image: const AssetImage(
+                          "assets/images/imbox.png",
                         ),
                       ),
                     ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Expanded(
+                        child: Column(
+                          children: [
+                            title("Welcome"),
+                            mediumText("Please Enter You ID"),
+                          ],
+                        )),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Image(
+                                image: const AssetImage(
+                                  "assets/images/card.png",
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Image(
+                                image: const AssetImage(
+                                  "assets/images/phone.png",
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Image(
+                                image: const AssetImage(
+                                  "assets/images/finger_print.jpg",
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
+              );
+            }
+            return Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Image(
+                      image: const AssetImage(
+                        "assets/images/imbox.png",
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                      child: Column(
+                        children: [
+                          title("Welcome"),
+                          mediumText("Please Enter You ID"),
+                        ],
+                      )),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: InkWell(
+                              onTap: () {
+                                showErrorDialog(context);
+                              },
+                              child: Image(
+                                image: const AssetImage(
+                                  "assets/images/card.png",
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Image(
+                              image: const AssetImage(
+                                "assets/images/phone.png",
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Image(
+                              image: const AssetImage(
+                                "assets/images/finger_print.jpg",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
+            );
+          },
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
   }
 }
