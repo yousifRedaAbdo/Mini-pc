3ad9436b91d4c6c424fc72476955303e8566b808
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-02-13 11:34:45 +0200

    Update dio settings and open locker function

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -1,78 +1,64 @@
 import 'package:dio/dio.dart';
 
-class dioHelper
-{
+class dioHelper {
   static late Dio dio;
-  static int()
-  {
-    dio = Dio(
-        BaseOptions(
-          baseUrl:'http://107.21.192.50:8000/api/',
-          receiveDataWhenStatusError: true,
-        )
-    );
+  static int() {
+    dio = Dio(BaseOptions(
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      receiveDataWhenStatusError: true,
+    ));
   }
+
   static Future<Response> getdata({
     required String url,
-    Map <String,dynamic>? quary,
+    Map<String, dynamic>? quary,
     String? token,
-  })async
-  {
+  }) async {
     dio.options.headers = {
       'Content-Type': 'application/json',
       'Authorization': 'Token $token'
     };
     return await dio.get(url, queryParameters: quary);
   }
+
   static Future<Response> postdata({
     required String url,
-    Map <String,dynamic>? quary,
-    required Map <String,dynamic>? data,
+    Map<String, dynamic>? quary,
+    required Map<String, dynamic>? data,
     String? token,
-  })async{
-    dio.options.headers ={
+  }) async {
+    dio.options.headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Token $token'
-    } ;
-    return dio.post(
-        url,
-        queryParameters: quary,
-        data: data
-    );
+      // 'Authorization': 'Token $token'
+    };
+    return dio.post(url, queryParameters: quary, data: data);
   }
+
   static Future<Response> putdata({
     required String url,
-    Map <String,dynamic>? quary,
-    required Map <String,dynamic>? data,
+    Map<String, dynamic>? quary,
+    required Map<String, dynamic>? data,
     String lang = 'en',
     String? token,
-  })async{
-    dio.options.headers ={
+  }) async {
+    dio.options.headers = {
       'Content-Type': 'application/json',
       'lang': lang,
       'Authorization': 'Token $token'
-    } ;
-    return dio.put(
-        url,
-        queryParameters: quary,
-        data: data
-    );
+    };
+    return dio.put(url, queryParameters: quary, data: data);
   }
 
   static Future<Response> postdataLogin({
     required String url,
-    Map <String,dynamic>? quary,
-    required Map <String,dynamic>? data,
+    Map<String, dynamic>? quary,
+    required Map<String, dynamic>? data,
     String? token,
-  })async{
-    dio.options.headers ={
+  }) async {
+    dio.options.headers = {
       'Content-Type': 'application/json',
-     // 'Authorization': token
-    } ;
-    return dio.post(
-        url,
-        queryParameters: quary,
-        data: data
-    );
+      // 'Authorization': token
+    };
+    return dio.post(url, queryParameters: quary, data: data);
   }
 }
--- lib/services/provider.dart
+++ lib/services/provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mini_pc/services/dio.dart';
 
 class DataProvider extends ChangeNotifier {
-
   bool isLoading = false;
 
   void setLoading(bool value) {
@@ -13,15 +12,17 @@ class DataProvider extends ChangeNotifier {
   openLocker({
     required String card_id,
     required TextEditingController controller,
-    required context
+    required context,
   }) {
+    // print(card_id);
     dioHelper.postdata(
-        url: 'open-locker/',
-        data: {
-          "card_id": "38882993004"
-        }).then((value) {
-          setLoading(false);
-          controller.clear();
+      url: 'open-locker/',
+      data: {
+        "card_id": card_id,
+      },
+    ).then((value) {
+      setLoading(false);
+      controller.clear();
       notifyListeners();
     }).catchError((erorr) {
       setLoading(false);
