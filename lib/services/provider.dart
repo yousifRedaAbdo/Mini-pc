6ca3424c1ce033e563d8f4f16a9816cc516970ca
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2024-02-15 16:26:28 +0200

    Add dialog message in error case

--- lib/services/provider.dart
+++ lib/services/provider.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:mini_pc/services/dio.dart';
 
+showMessageDialog(
+  BuildContext context,
+  String data,
+) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Icon(
+          Icons.lock_rounded,
+          color: Colors.red,
+          size: 55.0,
+        ),
+        content: Text(
+          data,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      );
+    },
+  );
+
+  // Set a timer to close the dialog after a certain period (e.g., 3 seconds)
+  Future.delayed(Duration(seconds: 6), () {
+    Navigator.of(context).pop(); // Close the dialog
+  });
+}
+
 class DataProvider extends ChangeNotifier {
   // bool isLoading = false;
   //
@@ -21,10 +50,13 @@ class DataProvider extends ChangeNotifier {
         "card_id": card_id,
       },
     ).then((value) {
-      controller.clear();
       notifyListeners();
     }).catchError((erorr) {
       print(erorr.toString());
+      showMessageDialog(
+          context, 'Something went wrong, please try again later!');
+      notifyListeners();
     });
+    controller.clear();
   }
 }
